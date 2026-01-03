19804ee353c8855b42f635f08d82c33554104cad
Mohan Lal <mhnuk2007@gmail.com>
2026-01-03 22:44:03 +0500

    Add @for loop and userDetails array with signals

--- control-structures/src/app/app.ts
+++ control-structures/src/app/app.ts
@@ -5,21 +5,28 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('control-structures');
 
   isLogin = signal(true);
   show = signal(true);
-  status=signal('progress');
-  users = signal(['honey', 'sunny', 'mukesh', 'hitesh', 'sandeep'])
+  status = signal('progress');
+  users = signal(['honey', 'sunny', 'mukesh', 'hitesh', 'sandeep']);
+  userDetails = signal([
+    { id: 1, name: 'honey', surname: 'chauhan', email: 'honey@example.com' },
+    { id: 2, name: 'sunny', surname: 'chauhan', email: 'sunny@example.com' },
+    { id: 3, name: 'mukesh', surname: 'chauhan', email: 'mukesh@example.com' },
+    { id: 4, name: 'hitesh', surname: 'chauhan', email: 'hitesh@example.com' },
+    { id: 5, name: 'sandeep', surname: 'chauhan', email: 'sandeep@example.com' },
+  ]);
 
-  handleLogin(state: boolean){
+  handleLogin(state: boolean) {
     this.isLogin.set(state);
   }
 
-  handleStatus(event: Event){
+  handleStatus(event: Event) {
     let target = event.target as HTMLSelectElement;
     this.status.set(target.value);
   }
