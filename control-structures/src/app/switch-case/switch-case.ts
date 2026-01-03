3d7634fe397da98d6049ae1cc4617a25de1ae4c7
Mohan Lal <mhnuk2007@gmail.com>
2026-01-04 00:08:31 +0500

    Create SwitchCaseComponent for control flow statement tutorial

---
+++ control-structures/src/app/switch-case/switch-case.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-switch-case',
+  imports: [],
+  templateUrl: './switch-case.html',
+  styleUrl: './switch-case.css',
+})
+export class SwitchCase {
+
+}
