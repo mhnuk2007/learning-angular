67c84541b9b65aff2db90608eef343f8fa844c23
Mohan Lal <mhnuk2007@gmail.com>
2026-01-03 15:21:07 +0500

    applied project level (global) styling

--- style-options/src/app/app.ts
+++ style-options/src/app/app.ts
@@ -1,9 +1,11 @@
+import { UserData } from './user-data/user-data';
+import { AdminData } from './admin-data/admin-data';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AdminData, UserData],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
