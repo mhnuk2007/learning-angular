a8bb3b0a05d51a07d78cee51306943226a0ce70e
Mohan Lal <mhnuk2007@gmail.com>
2026-01-04 15:08:41 +0500

    Update signals getter-setter with object-based two-way binding

--- two-way-data-binding/src/app/signals-getter-setter/signals-getter-setter.ts
+++ two-way-data-binding/src/app/signals-getter-setter/signals-getter-setter.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { FormsModule } from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signals-getter-setter',
@@ -8,15 +8,29 @@ import { FormsModule } from "@angular/forms";
   styleUrl: './signals-getter-setter.css',
 })
 export class SignalsGetterSetter {
-  _city = signal('Umerkot')
+  _city = signal('Umerkot');
+
+  userDetails = signal({
+    name: 'Honey Chauhan',
+    school: 'spsc',
+    email: 'honey@example.com',
+  })
 
   get city() {
-    return this._city()
+    return this._city();
   }
   set city(value: string) {
-    this._city.set(value)
+    this._city.set(value);
   }
 
+  get userSchool() {
+    return this.userDetails().school;
+  }
 
-
+  set userSchool(value: string) {
+    this.userDetails.update((data) => ({
+      ...data,
+      school: value,
+    }));
+  }
 }
