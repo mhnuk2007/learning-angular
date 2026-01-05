e91ebd90497de9f0f33bfe75ccbd8a09d4fd9954
Mohan Lal <mhnuk2007@gmail.com>
2026-01-05 22:20:29 +0500

    Add input-example as a regular folder

---
+++ input-example/src/app/app.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+import {User} from './user';
+
+@Component({
+  selector: 'app-root',
+  template: ` <app-user name = "Honey" occupation="Angular Developer" /> `,
+  imports: [User],
+})
+export class App {
+
+
+
+
+
+}
