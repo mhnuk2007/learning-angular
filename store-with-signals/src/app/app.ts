9d9dd429045d386e3175b88744e3cd4a5c5bbd15
Mohan Lal <mhnuk2007@gmail.com>
2026-01-05 23:30:34 +0500

    Update app and add core folder with counter service

--- store-with-signals/src/app/app.ts
+++ store-with-signals/src/app/app.ts
@@ -1,5 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CounterService} from './core/store/counter';
+
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('store-with-signals');
+
+  constructor(public counterService: CounterService){}
 }
---
+++ store-with-signals/src/app/core/store/counter.ts
@@ -0,0 +1,21 @@
+import { Injectable, signal, Signal } from "@angular/core";
+
+@Injectable({
+  providedIn: 'root',
+
+})
+
+export class CounterService {
+  count = signal(0);
+
+  increment(){
+    this.count.update(val => val + 1);
+  }
+  decrement(){
+    this.count.update(val => val - 1);
+  }
+  reset(){
+    this.count.set(0);
+  }
+
+}
