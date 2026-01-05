221eed55ec43016ed3796e9ed856332eb1d64b6d
Mohan Lal <mhnuk2007@gmail.com>
2026-01-05 19:57:03 +0500

    update...

--- angular-routing/src/app/app.routes.ts
+++ angular-routing/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
 import { Login } from './pages/login/login';
 import { Profile } from './pages/profile/profile';
+import { PageNotFound } from './pages/page-not-found/page-not-found';
 
 export const routes: Routes = [
   {path: '', component: Home},
   {path: 'about', component: About},
   {path: 'login', component: Login},
   {path: 'profile', component: Profile},
+  {path: '**', component: PageNotFound}
 
 ];
--- angular-routing/src/app/app.ts
+++ angular-routing/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { Header } from "./components/header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
