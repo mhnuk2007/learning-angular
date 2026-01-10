1063ec80b4fa9b975f55db2ff717fb047985288d
Mohan Lal <mhnuk2007@gmail.com>
2026-01-10 17:48:55 +0500

    Add template driven form with signals

--- angular-forms/src/app/app.ts
+++ angular-forms/src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-forms');
+  name = signal('');
+  email = signal('');
+
+  submitForm() {
+    console.log('Name:', this.name());
+    console.log('Email:', this.email());
+  }
+
 }
