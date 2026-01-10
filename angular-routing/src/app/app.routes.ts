7a96842f1e8694356244fa5b9aaf552ae1124418
Mohan Lal <mhnuk2007@gmail.com>
2026-01-10 16:43:18 +0500

    Apply dynamic component loading

--- angular-routing/src/app/app.routes.ts
+++ angular-routing/src/app/app.routes.ts
@@ -1,21 +1,6 @@
 import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
-import { About } from './pages/about/about';
-import { Login } from './pages/login/login';
-import { Profile } from './pages/profile/profile';
-import { PageNotFound } from './pages/page-not-found/page-not-found';
-import { Users } from './pages/users/users';
-import { UserDetails } from './pages/user-details/user-details';
 
 export const routes: Routes = [
-  {path: '', component: Home},
-  {path: 'about', component: About},
-  {path: 'login', component: Login},
-  {path: 'profile', component: Profile},
-  {path: 'profile/:id', component: Profile},
-  {path: 'users', component: Users},
-  {path: 'user-details', component: UserDetails},
-  {path: 'user-details/:id', component: UserDetails},
-  {path: '**', component: PageNotFound}
+  
 
 ];
--- angular-routing/src/app/app.ts
+++ angular-routing/src/app/app.ts
@@ -1,13 +1,24 @@
-import { Component, signal } from '@angular/core';
+
+import { Component, signal, ViewChild, ViewContainerRef } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { Header } from "./components/header/header";
+import { UserDetails } from "./user-details/user-details";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, UserDetails],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  @ViewChild('container', {read: ViewContainerRef})
+  container!: ViewContainerRef
   protected readonly title = signal('angular-routing');
+
+
+  async loadUserDetails(){
+    this.container?.clear();
+    console.log('loadUserDetails');
+    const {UserDetails} = await import('./user-details/user-details')
+    this.container?.createComponent(UserDetails)
+  }
 }
