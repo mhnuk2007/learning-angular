418db40f83dc46d71fed28551c5c2d713a590563
Mohan Lal <mhnuk2007@gmail.com>
2026-01-16 21:49:35 +0500

    Add lifecycle hook ngOnInit

--- course-booking-system/src/app/courses-list/courses-list.ts
+++ course-booking-system/src/app/courses-list/courses-list.ts
@@ -9,6 +9,10 @@ import { Component } from '@angular/core';
 export class CoursesList {
   title = 'Available Courses';
 
+  ngOnInit(): void {
+    console.log('CoursesList component initialized.');
+  }
+
   courses = [
     {id: 1, name: 'Introduction to Angular', description:'Learn the basics of Angular framwork.', price: 49, date: '2025-03-01', soldOut: false, imgUrl:'angularlogo.svg' },
     {id: 2, name: 'Advance Angular', description:'Deep dive into advanced Angular topics.', price: 99, date: '2025-04-15', soldOut: true, imgUrl:'angularlogo.svg' },
