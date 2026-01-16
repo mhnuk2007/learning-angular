1bdc4ea951d233f90719a86f9ea28b084086cea8
Mohan Lal <mhnuk2007@gmail.com>
2026-01-16 22:32:30 +0500

    lesson 2 challenge complete

--- course-booking-system/src/app/courses-list/courses-list.ts
+++ course-booking-system/src/app/courses-list/courses-list.ts
@@ -15,8 +15,8 @@ export class CoursesList {
   }
 
   courses = [
-    {id: 1, name: 'Introduction to Angular', description:'Learn the basics of Angular framwork.', price: 49, date: '2025-03-01', soldOut: false, onSale: false, imgUrl:'angularlogo.svg' },
-    {id: 2, name: 'Advance Angular', description:'Deep dive into advanced Angular topics.', price: 99, date: '2025-04-15', soldOut: true, onSale: true, imgUrl:'angularlogo.svg' },
+    {id: 1, name: 'Introduction to Angular', description:'Learn the basics of Angular framwork.', price: 49, date: '2025-03-01', soldOut: false, onSale: true, imgUrl:'angularlogo.svg' },
+    {id: 2, name: 'Advance Angular', description:'Deep dive into advanced Angular topics.', price: 99, date: '2025-04-15', soldOut: true, onSale: false, imgUrl:'angularlogo.svg' },
     {id: 3, name: 'Introduction to RxJS', description:'Learn reactive programming with RxJS.', price: 39, date: '2025-05-10', soldOut: false, onSale: false, imgUrl:'rxjs.png' },
   ]
 
