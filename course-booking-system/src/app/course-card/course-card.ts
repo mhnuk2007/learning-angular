9a4ca492946ee7063e8e054d020e0d1f3c053f21
Mohan Lal <mhnuk2007@gmail.com>
2026-01-17 00:48:48 +0500

    Implement parent-child communication using @Input and @Output

--- course-booking-system/src/app/course-card/course-card.ts
+++ course-booking-system/src/app/course-card/course-card.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-course-card',
-  imports: [],
+  imports: [CurrencyPipe, DatePipe],
   templateUrl: './course-card.html',
   styleUrl: './course-card.css',
 })
 export class CourseCard {
+  @Input() course: any;
+  @Output() courseBooked = new EventEmitter<any>();
 
+  onBookCourse(): void {
+    this.courseBooked.emit(this.course);
+  }
 }
--- course-booking-system/src/app/courses-list/courses-list.ts
+++ course-booking-system/src/app/courses-list/courses-list.ts
@@ -1,9 +1,9 @@
-import { CurrencyPipe, DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { CourseCard } from "../course-card/course-card";
 
 @Component({
   selector: 'app-courses-list',
-  imports: [CurrencyPipe, DatePipe],
+  imports: [CourseCard],
   templateUrl: './courses-list.html',
   styleUrl: './courses-list.css',
 })
@@ -19,10 +19,11 @@ export class CoursesList {
     {id: 2, name: 'Advance Angular', description:'Deep dive into advanced Angular topics.', price: 99, date: '2025-04-15', soldOut: true, onSale: false, imgUrl:'angularlogo.svg' },
     {id: 3, name: 'Introduction to RxJS', description:'Learn reactive programming with RxJS.', price: 39, date: '2025-05-10', soldOut: false, onSale: false, imgUrl:'rxjs.png' },
   ]
-  
 
-  viewDetails(title: string): void {
-    alert(`You have selected the course: ${title}`);
-  }
+  onCourseBooked(course: any): void {
+    alert('Course booked successfully: ' + course.name);
+  };
+
+
 
 }
