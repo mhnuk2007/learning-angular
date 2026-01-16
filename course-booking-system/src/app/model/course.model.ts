581f0f52ba89c10160fb07d3b7223679fdfd5909
Mohan Lal <mhnuk2007@gmail.com>
2026-01-17 02:56:43 +0500

    Add model

---
+++ course-booking-system/src/app/model/course.model.ts
@@ -0,0 +1,12 @@
+export interface Course {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  date?: string;
+  imgUrl?: string;
+  soldOut?: boolean;
+  onSale?: boolean;
+  
+
+}
