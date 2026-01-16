d2d29c4038765d6f55365f22576d8121d2609099
Mohan Lal <mhnuk2007@gmail.com>
2026-01-16 20:22:36 +0500

    Handle view details button click for courses

--- course-booking-system/src/app/courses-list/courses-list.ts
+++ course-booking-system/src/app/courses-list/courses-list.ts
@@ -14,4 +14,8 @@ export class CoursesList {
     {id: 2, name: 'Advance Angular', description:'Deep dive into advanced Angular topics.', price: 99, date: '2025-04-15', soldOut: true, imgUrl:'angularlogo.svg' },
   ]
 
+  viewDetails(title: string){
+    alert(`You have selected the course: ${title}`);
+  }
+
 }
