f3851fa6d475e12960642ec1c4c8ae3ecc0a89e0
Mohan Lal <mhnuk2007@gmail.com>
2026-01-18 02:31:47 +0500

    Add routing

--- course-booking-system/src/app/app.ts
+++ course-booking-system/src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkWithHref, RouterLinkActive } from '@angular/router';
 import { CoursesList } from "./courses-list/courses-list";
 
 @Component({
   selector: 'app-root',
-  imports: [CoursesList],
+  imports: [CoursesList, RouterOutlet, RouterLinkWithHref, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
