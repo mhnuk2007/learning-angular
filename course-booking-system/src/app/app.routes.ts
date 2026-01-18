7c825a61b84e9ca0bd0ddad70dac3fb5612bcc24
Mohan Lal <mhnuk2007@gmail.com>
2026-01-18 15:20:29 +0500

    Add reactive form

--- course-booking-system/src/app/app.routes.ts
+++ course-booking-system/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { CoursesList } from './courses-list/courses-list';
 import { CourseDetails } from './course-details/course-details';
 import { About } from './about/about';
+import { SignUpForm } from './sign-up-form/sign-up-form';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'courses', pathMatch: 'full' },
   { path: 'courses', component: CoursesList },
   { path: 'courses/:id', component: CourseDetails},
-  { path: 'about', component: About}
+  { path: 'about', component: About},
+  { path: 'sign-up', component: SignUpForm}
 
 ];
--- course-booking-system/src/app/sign-up-form/sign-up-form.ts
+++ course-booking-system/src/app/sign-up-form/sign-up-form.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CourseService } from '../services/course';
+import { Course } from '../model/course.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sign-up-form',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './sign-up-form.html',
   styleUrl: './sign-up-form.css',
 })
 export class SignUpForm {
+  signUpForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private courseService: CourseService){}
+  courses = signal<Course[] | null>(null);
+  ngOnInit(): void {
+    this.signUpForm = this.fb.group({
+      name:[''],
+      email: [''],
+      enrolledCourseIds:['']
+    })
+
+    this.courseService.getCourses().subscribe({
+      next: (data) => {
+        this.courses.set(data);
+        console.log('Courses loaded successfully:', data);
+      },
+      error: (error) => {
+        console.error('Error loading courses:', error);
+      }
+    })
+  }
+
+
 
 }
