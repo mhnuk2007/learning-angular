f569b46b53e2547ac9329770ab6ebd0ab41664a2
Mohan Lal <mhnuk2007@gmail.com>
2026-01-18 16:21:18 +0500

    Update add course form component

--- course-booking-system/src/app/add-course-form/add-course-form.ts
+++ course-booking-system/src/app/add-course-form/add-course-form.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Course } from '../model/course.model';
+import { CourseService } from '../services/course';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-course-form',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './add-course-form.html',
   styleUrl: './add-course-form.css',
 })
 export class AddCourseForm {
+  addCourseForm!: FormGroup;
+  course = signal<Course | null>(null);
+  submitted = false;
 
+  constructor(
+    private fb: FormBuilder,
+    private courseService: CourseService,
+  ) {}
+
+  ngOnInit(): void {
+    this.addCourseForm = this.fb.group({
+      title: [''],
+      description: [''],
+      price: [''],
+      date: [''],
+      imgUrl: [''],
+      soldOut: [false],
+      onSale: [false],
+    });
+  }
+
+  onSubmit(): void {
+    const course: Course = {
+      id: 0,
+      title: this.addCourseForm.value.title,
+      description: this.addCourseForm.value.description,
+      price: Number(this.addCourseForm.value.price),
+      date: this.addCourseForm.value.date,
+      imgUrl: this.addCourseForm.value.imgUrl,
+      soldOut: this.addCourseForm.value.soldOut,
+      onSale: this.addCourseForm.value.onSale,
+    };
+    this.courseService.addCourse(course).subscribe({
+      next: (data) => {
+        console.log('Course added successfully:', data);
+      },
+      error: (error) => {
+        console.error('Error adding course:', error);
+      }
+    })
+
+
+    this.submitted = true;
+  }
 }
